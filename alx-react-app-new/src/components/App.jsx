a343c4ae4ece3d7e691cdc54030b72e0ea1b1c0e
Hopizy <147936629+Hopizy@users.noreply.github.com>
2025-07-25 23:39:09 +0100

    Add files via upload

--- alx-react-app-new/src/components/App.jsx
+++ alx-react-app-new/src/components/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Header from './Header';
+import UserProfile from './UserProfile';
 import MainContent from './MainContent';
 import Footer from './Footer';
-
+import Counter from './Counter';
 function App() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <UserProfile name="John Doe" age={28} bio="Frontend developer and travel enthusiast." />
+      <MainContent />
+      <Counter />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
