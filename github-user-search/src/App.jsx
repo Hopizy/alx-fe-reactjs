58c7a6315b0cf5903ee41c23f028a7a78477848c
Hopizy <147936629+Hopizy@users.noreply.github.com>
2025-08-03 21:01:25 +0100

    Add github-user-search folder

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search App</h1>
+      {/* Search component will be added here later */}
+    </div>
+  );
+}
+
+export default App;
---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const token = import.meta.env.VITE_APP_GITHUB_API_KEY;
+
+const fetchUserData = async (username) => {
+  const response = await axios.get(
+    `https://api.github.com/users/${username}`,
+    {
+      headers: {
+        Authorization: `token ${token}`,
+      },
+    }
+  );
+  return response.data;
+};
+
+export default fetchUserData;
