24096fb5b98cac254c52ad0a2ed78ff0b4f86585
Hopizy <147936629+Hopizy@users.noreply.github.com>
2025-08-05 21:55:58 +0100

    Add files via upload

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const fetchUserData = async (username) => {
+export const fetchUserData = async (username) => {
   const response = await axios.get(`https://api.github.com/users/${username}`);
   return response.data;
 };
-
-export default fetchUserData;
