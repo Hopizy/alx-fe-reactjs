e524ca29de41cca0f1fd3c2ab55988aa2338e8bd
Hopizy <147936629+Hopizy@users.noreply.github.com>
2025-08-06 22:18:06 +0100

    Add files via upload

--- github-user-search/src/main.jsx
+++ github-user-search/src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // ✅ this line is required
+import App from './App';
+import './index.css'; //import Tailwind css here
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
